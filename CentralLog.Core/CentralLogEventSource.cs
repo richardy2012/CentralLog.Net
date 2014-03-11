10c73daba7b22e439a1d9cb4fb197b4f98e9abe9
Epstone <Patrickeps@gmx.de>
2014-03-11 21:10:34 +0100

    improved logger interface

--- CentralLog.Core/CentralLogEventSource.cs
+++ CentralLog.Core/CentralLogEventSource.cs
@@ -11,58 +11,62 @@ namespace CentralLog.Core
         public static ICentralLogEventSource Log { get { return CentralLogEventSource.Log; } }
     }
 
-
-    public interface ICentralLogEventSource
-    {
-        void LogInfo(string message);
-        void LogWarning(string message);
-        void LogError(Exception ex, string message = null);
-        void LogData(object data);
-    }
-
     // This code belongs in the process generating the events.  They are samples 
     [EventSource(Name = GlobalDefines.EVENT_SOURCE_NAME)]     // This is the name of my eventSource outside my program.  
-    sealed class CentralLogEventSource : EventSource, ICentralLogEventSource
+    sealed class CentralLogEventSource : EventSource, ICentralLogEventSource, ICentralLogEventSource
     {
-
         // Typically you only create one EventSource and use it throughout your program.  Thus a static field makes sense.  
         public static CentralLogEventSource Log = new CentralLogEventSource();
 
         [NonEvent]
-        public void LogData(object data)
+        public void LogData(object data, Guid? jobId = null)
         {
             this.LogData("todo");
         }
 
         [NonEvent]
-        public void LogError(Exception ex, string message = null)
+        public void LogError(Exception ex, string message = null, Guid? jobId = null)
         {
             this.LogError("todo");
         }
 
         [Event(1)]        
-        public void LogInfo(string message)
+        public void LogInfo(string message, Guid? jobId = null)
         {
             WriteEvent(1, message);
         }
 
-        [Event(2)]        
-        public void LogWarning(string message)
+        [Event(2)]
+        public void LogWarning(string message, Guid? jobId = null)
         {
             throw new NotImplementedException();
         }
 
         [Event(3)]
-        public void LogError(string ex, string message = null)
+        public void LogError(string ex, string message = null, Guid? jobId = null)
         {
             throw new NotImplementedException();
         }
 
         [Event(4)]
-        public unsafe void LogData(string json)
+        public void LogData(string json, Guid? jobId = null)
         {
             throw new NotImplementedException();
         }
+
+        [Event(5)]
+        public void LogJobStart(Guid jobId, string message)
+        {
+            
+        }
+
+        [Event(6)]
+        public void LogJobEnd(Guid jobId, string message)
+        {
+            
+        }
+
+      
     }
 
 
---
+++ CentralLog.Core/ICentralLogEventSource.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.Diagnostics.Tracing;
+
+namespace CentralLog.Core
+{
+    public interface ICentralLogEventSource
+    {
+        [NonEvent]
+        void LogData(object data, Guid? jobId = null);
+
+        [NonEvent]
+        void LogError(Exception ex, string message = null, Guid? jobId = null);
+
+        [Event(1)]
+        void LogInfo(string message, Guid? jobId = null);
+
+        [Event(2)]
+        void LogWarning(string message, Guid? jobId = null);
+
+        [Event(3)]
+        void LogError(string ex, string message = null, Guid? jobId = null);
+
+        [Event(4)]
+        void LogData(string json, Guid? jobId = null);
+
+        [Event(5)]
+        void LogJobStart(Guid jobId, string message);
+
+        [Event(6)]
+        void LogJobEnd(Guid jobId, string message);
+    }
+}
